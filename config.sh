364824dfd004154045ab9e454ee68cbdd8d25d6a
NotZeetaa <rodrigo2005contente@gmail.com>
2024-01-30 19:46:19 +0000

    DroidConsole: Added IO Wait Boost option
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- config.sh
+++ config.sh
@@ -6,6 +6,7 @@ S2=/sys/devices/system/cpu/cpufreq/schedutil
 SC=/sys/devices/system/cpu/cpu0/cpufreq/schedutil
 GPU_THROTTLING=
 CPU_RATELIMITS=
+IOWAIT_BOOST=
 
 if [ "$GPU_THROTTLING" -eq 1 ]; then
     echo "1" > $KGSL/throttling
@@ -22,6 +23,26 @@ if [ "$CPU_RATELIMITS" -eq 1 ]; then
         do
             echo "500" > "${cpu}/up_rate_limit_us"
             echo "20000" > "${cpu}/down_rate_limit_us"
+        done
+    fi
+fi
+
+if [ "$IOWAIT_BOOST" -eq 1 ]; then
+    if [ -d $S2 ]; then
+        echo "1" > $S2/iowait_boost_enable
+    elif [ -e $SC ]; then
+        for cpu in /sys/devices/system/cpu/*/cpufreq/schedutil
+        do
+            echo "1" > "${cpu}/iowait_boost_enable"
+        done
+    fi
+elif [ "$IOWAIT_BOOST" -eq 0 ]; then
+    if [ -d $S2 ]; then
+        echo "0" > $S2/iowait_boost_enable
+    elif [ -e $SC ]; then
+        for cpu in /sys/devices/system/cpu/*/cpufreq/schedutil
+        do
+            echo "0" > "${cpu}/iowait_boost_enable"
         done
     fi
 fi
