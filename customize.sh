7a1a9c47797175eeb9460e9c4793e91c6a7b118e
NotZeetaa <rodrigo2005contente@gmail.com>
2024-03-11 20:41:05 +0000

    tree: Switch to shell script extension
    * Somehow without this, script doesn't get called
    
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- customize.sh
+++ customize.sh
@@ -8,7 +8,7 @@ set -x
  abort "- Unsupported API version: $API"
 
 mkdir -p $MODPATH/system/bin
-mv -f $MODPATH/DroidConsole $MODPATH/system/bin/DroidConsole
+mv -f $MODPATH/DroidConsole.sh $MODPATH/system/bin/DroidConsole.sh
 
 # Clean up
 ui_print "  Cleaning obsolete files"
@@ -23,4 +23,4 @@ find $MODPATH/* -maxdepth 0 \
 # Settings dir and file permission
 ui_print "  Settings permissions"
 set_perm_recursive $MODPATH 0 0 0755 0755
-set_perm $MODPATH/system/bin/DroidConsole 0 2000 0755
+set_perm $MODPATH/system/bin/DroidConsole.sh 0 2000 0755
